471b7c0ea243f8a082d80415920eaaecd5efa178
Nadia <nuriyeerdogan38@gmail.com>
2026-02-25 18:35:51 +0100

    refactor: remove default weather template

--- WhistleblowerA_N/Program.cs
+++ WhistleblowerA_N/Program.cs
@@ -1,3 +1,5 @@
+using WhistleblowerA_N.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddSingleton<IReportRepository, InMemoryReportRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,5 +22,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
