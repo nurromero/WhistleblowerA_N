185893b11a750078f4024cc10da63f25266ca036
Nadia <nuriyeerdogan38@gmail.com>
2026-02-25 19:21:58 +0100

    feat: createreportrequest. how its supposed to look like for the userinput

---
+++ WhistleblowerA_N/Domain/CreateReportRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace WhistleblowerA_N.Domain
+{
+    public class CreateReportRequest
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
