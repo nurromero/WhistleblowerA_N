360c8bb433c2060777aa122c2a18ce3404111969
asya-xo <gulsumerd38@gmail.com>
2026-02-26 18:48:54 +0100

    feat: add anonymous report submission and tracking status endpoints

---
+++ WhistleblowerA_N/Controllers/ReportsController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhistleblowerA_N.Domain;
+using WhistleblowerA_N.Repositories;
+
+namespace WhistleblowerA_N.Controllers
+{
+    //Setting up the controller for the API, this will be used to handle incoming HTTP requests related to reports. It will use the IReportRepository to interact with the data layer.
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IReportRepository _repository;
+
+        public ReportsController(IReportRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        //We're returning only the tracking token
+        [HttpPost]
+        public async Task<IActionResult> CreateReport([FromBody] CreateReportRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Title) ||
+                string.IsNullOrWhiteSpace(request.Description))
+            {
+                return BadRequest("Title and Description are required.");
+            }
+
+            var report = new Report
+            {
+                Title = request.Title,
+                Description = request.Description
+            };
+
+            var createdReport = await _repository.CreateReportAsync(report);
+
+            return Ok(new
+            {
+                trackingToken = createdReport.TrackingToken
+            });
+        }
+
+        [HttpGet("{trackingToken}")]
+        public async Task<IActionResult> GetReportStatus(string trackingToken)
+        {
+            var report = await _repository.GetReportByTrackingTokenAsync(trackingToken);
+            if (report == null)
+            {
+                return NotFound("Report not found.");
+            }
+            return Ok(new
+            {
+                status = report.Status.ToString(),
+                dateReported = report.DateReported
+            });
+
+
+        }
+    }
+}
+
--- WhistleblowerA_N/Program.cs
+++ WhistleblowerA_N/Program.cs
@@ -6,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IReportRepository, InMemoryReportRepository>();
 var app = builder.Build();
@@ -14,7 +15,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
